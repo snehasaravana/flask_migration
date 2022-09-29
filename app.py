0a314046066d7ea1691cb189717ab3d178511dd6
snehasara06 <sneha.sara.06@gmail.com>
2022-09-29 12:55:55 +0530

    API CALL

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask,request
+from flask_migrate import Migrate
+from models import db, User
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost:5432/userlist"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+migrate = Migrate(app, db)
+ 
+@app.route('/users',methods=['POST','GET'])
+def users():
+
+    if request.method=="POST":
+        if request.is_json:
+            post_data=request.get_json()
+            new_user=User(
+                name=post_data['name'],
+                email=post_data['email'],
+                phone=post_data['phone'],
+                city=post_data['city'],
+                country=post_data['country'],
+                dob=post_data['dob'])
+            db.session.add(new_user)
+            db.session.commit()
+            return {"message":  f"user {new_user.name} has been added successfully."},201
+        else:
+             return {"error":"Check data format"},403
+        
+
+    elif request.method=='GET':
+        users=User.query.all()
+        results=[{
+            "name":user.name,
+            "email":user.email,
+            "phone":user.phone,
+            "city":user.city,
+            "country":user.country,
+            "dob":user.dob
+        } for user in users]
+    return {"count":len(results),"users":results},200
+@app.route('/users/<user_id>',methods=['GET','PUT',"DELETE"])
+def list(user_id):
+    user=User.query.get_or_404(user_id)
+    if request.method=='GET':
+        response={
+            "name":user.name,
+            "email":user.email,
+            "phone":user.phone,
+            "city":user.city,
+            "country":user.country,
+            "dob":user.dob
+        }
+        return {"message":'SUCCESS',"user":response},200
+    elif request.method=='PUT':
+        data=request.get_json()
+        user.name=data['name']
+        user.email=data['email']
+        user.phone=data['phone']
+        user.city=data['city']
+        user.country=data['country']
+        user.dob=data['dob']
+        db.session.add(user)
+        db.session.commit()
+        return {"message": f" user {user.name} successfully updated"},200
+
+    elif request.method=='DELETE':
+        db.session.delete(user)
+        db.session.commit()
+        return{"message": f"user {user.name} deleted successfully."},200
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
---
+++ models.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+ 
+db = SQLAlchemy()
+ 
+class User(db.Model):
+    __tablename__ = 'userList'
+ 
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20))
+    email = db.Column(db.Text(30))
+    phone=db.Column(db.String(10))
+    city=db.Column(db.String(20))
+    country=db.Column(db.String(20))
+    dob=db.Column(db.Date)
+ 
+
+    def __init__(self, name,email,phone,city,country,dob):
+        self.name = name
+        self.age = email
+        self.phone=phone
+        self.city=city
+        self.country=country
+        self.dob=dob
+ 
+    def __repr__(self):
+        return f"{self.name}:{self.email}:{self.phone}:{self.city}:{self.country}:{self.dob}"
+
